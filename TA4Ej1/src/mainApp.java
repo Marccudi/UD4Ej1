3a003a38887bd81773ca9b0d5db706d0f97abfd0
Marccudi <marccudi@hotmail.es>
2022-07-06 22:07:49 +0200

    Ejercicio 1 acabado

---
+++ TA4Ej1/src/mainApp.java
@@ -0,0 +1,22 @@
+
+public class mainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 35;
+		int num2 = 14;
+		
+		int suma = num1 + num2;
+		int resta = num1 - num2;
+		int multi = num1 * num2;
+		int divi = num1 / num2;
+		int mod = num1 % num2;
+		System.out.println("Num1:"+num1+" Num2:"+num2);
+		System.out.println("Suma: "+suma);
+		System.out.println("Resta: "+resta);
+		System.out.println("Multiplicación: "+multi);
+		System.out.println("División: "+divi);
+		System.out.println("Módulo: "+mod);
+	}
+
+}
